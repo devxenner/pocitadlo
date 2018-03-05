56c9639143ccd639caf1b911cdbb62684ce45963
Your Name <36930355+devxenner@users.noreply.github.com>
2018-03-05 19:02:48 +0100

    Úprava, přepsání a přidání ochrany SQL injection

--- index.php
+++ index.php
@@ -1,21 +1,39 @@
 <?php include_once 'partials/header.php' ?>
 
 <?php
-	// Zkontroluji, zda přišel GET request na stránku.
-	if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-		// Pokud přišel GET request, vytáhnu si z DB současný počet zhlídnutí stránky
-		$query = $db->query('SELECT total_count FROM visitors');
 
-		// Uložím si celkový počet do proměnné result jako objekt
-		$result = $query->fetch(PDO::FETCH_OBJ);
+	/*
+	 * Získání počtu návštěv z DB
+	 */
 
-		// Navýším hodnotu celkového počtu zhlídnutí
-		$total_count = ++$result->total_count;
+	// Připravím si query pro spuštění, abych předešel SQL Injection
+	$query = $db->prepare( 'SELECT total_count FROM visitors' );
 
-		// Vložím do DB navýšený počet
-		$query = $db->query("UPDATE visitors 
-							SET total_count = '$total_count'");
-	}
+	// Spustím query
+	// PS. Nevím zda je nutné toto dělat pro SELECT? Ale vyzkouším si princip, jak to funguje.
+	$query->execute();
+
+	// Nyní si uložím to, co mi z DB přišlo do proměnné result jako objekt.
+	$result = $query->fetch(PDO::FETCH_OBJ);
+
+	// Získám konkrétní číslo zhlédnutí
+	$total_count = $result->total_count;
+
+
+
+	/*
+	 * Nahrání nového počtu zhlédnutí do DB
+	 */
+
+	// Připravím si query pro spuštění, abych předešel SQL Injection
+	$query = $db->prepare("
+		UPDATE visitors
+		SET total_count = :total_count
+	");
+
+	// Na mnou vyznačené místo jako "named placeholder" vložím o jedna zvýšený počet zhlédnutí
+	$query->execute( ['total_count' => $total_count + 1] );
+
 ?>
 
 	<div class="visitors">
